f953a83768d9d23178e31143f69ea4abd3c117d2
cobmin <jacob-huber@outlook.com>
2026-09-16 14:48:33 -0500

    Add Instagram to site social links

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,5 +2,6 @@ export const socialLinks = [
   { label: 'LinkedIn', icon: 'linkedin', href: 'https://www.linkedin.com/in/jacobhuberonline/' },
   { label: 'GitHub', icon: 'github', href: 'https://github.com/jacobhuberonline' },
   { label: 'Facebook', icon: 'facebook', href: 'https://www.facebook.com/jacobhuber.eth/' },
+  { label: 'Instagram', icon: 'instagram', href: 'https://www.instagram.com/jacobhuberonline/' },
   { label: 'Chess.com', icon: 'chess', href: 'https://www.chess.com/member/cobmin' },
 ] as const;
