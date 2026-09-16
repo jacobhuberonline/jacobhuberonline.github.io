750bc53456130816bd31c2914b52144d99c58049
cobmin <jacob-huber@outlook.com>
2026-09-15 22:43:27 -0500

    Show navigation on upward scroll and add social profiles

---
+++ src/data/socials.ts
@@ -0,0 +1,6 @@
+export const socialLinks = [
+  { label: 'LinkedIn', href: 'https://www.linkedin.com/in/jacobhuberonline/' },
+  { label: 'GitHub', href: 'https://github.com/jacobhuberonline' },
+  { label: 'Facebook', href: 'https://www.facebook.com/jacobhuber.eth/' },
+  { label: 'Chess.com', href: 'https://www.chess.com/member/cobmin' },
+];
